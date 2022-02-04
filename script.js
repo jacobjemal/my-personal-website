3f8b74b31da67b4fe95d28ef88eadb1771ebf43c
jacob jemal <jacobjemal7@gmail.com>
2022-02-04 11:27:05 -0500

    hello

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "My name is Jacob jemal, and this this is my first website.";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('input');
+
